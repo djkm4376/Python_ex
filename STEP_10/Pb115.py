599e1f1ad515c5e34b8a572e135a7c9c983ca136
Kkm <djkm4376@naver.com>
2022-09-14 18:42:25 +0900

    9/14

---
+++ STEP_10/Pb115.py
@@ -0,0 +1,4 @@
+Dahyun = {'height' : '158cm', 'age' : 21, 'birth' : 'May 28th', 'group' : 'twice'}
+
+print('Dahyun is',Dahyun['height'])
+print('Dahyun belongs to', Dahyun['group'])
---
+++ STEP_12/Ex122.py
@@ -0,0 +1,20 @@
+mySet = {1, 3, 5}
+print(mySet)
+mySet.add('A')
+print(mySet)
+print()
+
+print(mySet.pop())
+print(mySet)
+print(mySet.pop())
+mySet = {'apple', 'melon', 'strawberry','grape'}
+print(mySet)
+print()
+
+mySet.discard('strawberry')
+print(mySet)
+mySet.remove('grape')
+print(mySet)
+
+mySet.clear()
+print(mySet)
